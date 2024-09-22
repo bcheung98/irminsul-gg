413d69634c97edd1f4e787c5f374b32b6c47d476
Brian <briantcheung@gmail.com>
2024-09-21 23:15:42 -0700

    Added 'about' section

---
+++ src/About.tsx
@@ -0,0 +1,38 @@
+// MUI imports 
+import { useTheme } from "@mui/material/styles"
+import { Box, Typography } from "@mui/material"
+
+function About() {
+
+    const theme = useTheme()
+
+    return (
+        <Box
+            sx={{
+                display: "block",
+                width: "50%",
+                mx: "auto",
+                mb: "100px",
+                p: 5,
+                backgroundColor: `${theme.paper.backgroundColor}`,
+                color: `${theme.text.color}`,
+                borderRadius: "15px"
+            }}
+        >
+            <Typography variant="h4" sx={{ mb: "30px" }}>The story of Irminsul.GG</Typography>
+            <Typography>Back in March 2021, when I was first learning how to make websites using React.JS in a coding bootcamp, I decided that it would be a good idea to apply what I've learned to something I really enjoy. Of course, making a website for <a href="https://genshin.hoyoverse.com/en/home"><em>Genshin Impact</em></a>, which came out six months before, was the first thing that came to mind. I wanted to make a simple, single-page database website that showed all the characters and what materials they need to level up, and be able to filter them based off different attributes. At the time, there were only one or two other database websites that provided this kind of information for <em>Genshin Impact</em>, and popular sites such as <em>Project Amber</em> and <em>Hakush.in</em> didn't exist yet. Over the course of a week, I put together the website, and my creative brain named it "<a href="https://bcheung98.github.io/genshin-impact-reference-sheet/">Genshin Impact Reference Sheet</a>" (GIRS).</Typography><br />
+            <Typography>I kept updating the site as new characters were released, and when Version 2.0 of <em>Genshin Impact</em> released in July 2021, I decided to commemorate it with <a href="https://bcheung98.github.io/genshin-impact-reference-sheet-2.0/">GIRS 2.0</a>. GIRS 2.0 added a popup showing detailed character information displaying character talents and constellations, expanded filters, and a cleaner look from using Material UI. In addition, I implemnted react-redux to help centralize the application state of the site. Once again, I kept updating the site with each new character release, and over time added more features such as tables for character stats, ascension, talent scaling, and levelling.</Typography><br />
+            <Typography>Sometime in the middle of 2022, I wanted to add client-side routing to the site to enable multi-page browsing; this would allow me to add dedicated pages for characters, a section for view weapons, and much more. It was the last thing I needed to turn my little passion project into a full website, and my goal for GIRS 3.0. However, due to a <a href="https://create-react-app.dev/docs/deployment/#notes-on-client-side-routing">limitation of GitHub Pages and React Router</a>, I was unable to implement this.</Typography><br />
+            <Typography>In January 2023, I found a <a href="https://github.com/rafgraph/spa-github-pages">script</a> that got around GitHub Pages' limitation, and after figuring out how it works, began to work on implementing it to my site. I added pages for weapons, banners, and the newly-released TCG game mode. After finishing GIRS 3.0 revamp, I decided to name the website "Project Irminsul", after <em>Genshin Impact</em>'s <a href="https://genshin-impact.fandom.com/wiki/Irminsul">world tree</a> that serves as a <u>repository for all of the information</u> of the game's universe. (Note: there is no link to "Project Irminsul" because the repo associated with it is the same one serving genshin.irminsul.gg currently, and I took down the GitHub Pages link)</Typography><br />
+            <Typography>In April 2023, I started playing HoYoverse's newly-released <a href="https://hsr.hoyoverse.com/en-us/home"><em>Honkai: Star Rail</em></a>, and knew that I had to make a website for HSR as well. I finished the website at the end of June 2023, and named it "Project Stellaron". Project Stellaron's release came with a new feature in the ascension planner, and I also implented this planner to Project Irminsul as well.</Typography><br />
+            <Typography>I decided not to do a revamp of Project Irminsul when Version 4.0 of <em>Genshin Impact</em> released in August 2023, mainly because I didn't know what else I could add at the time. In May 2024, Kuro Games released <a href="https://wutheringwaves.kurogames.com/en/main"><em>Wuthering Waves</em></a>, which I began playing and also decided to make a website for it, naming it "Project Tacetite". In contrast to the previous two websites, I wrote Project Tacetite in TypeScript, which I had began learning at the time. I converted the entirety of Project Irminsul to TypeScript a few months later, and decided to make that my 4.0 revamp. I also added pages to all my sites to view Artifacts, Relics, and Echoes, for Genshin, HSR, and WuWa respectively.</Typography><br />
+            <Typography>In September 2024, I learned how to use some Amazon Web Services features, including S3, CloudFront, and Route 53. I realized that with <em>Genshin Impact</em>'s Version 5.0's release a few weeks prior, it would be the perfect time to migrate all my websites from GitHub Pages to CloudFront. In addition, I decided that I would create a central hub website that would serve as a portal for my three websites; Project Irminsul, Project Stellaron, and Project Tacetite would all be subdomains of the central website. I also wanted to come up with a brand name that would be the name of the central hub and the entire website collection. I inititally came up with "Elysia Project", named after my favorite <em>Honkai Impact 3rd</em> character, but decided to keep the "Irminsul" name, as it conveys the idea of an information database the best. The irminsul.gg domain was available, so I used that to complete the brand name of my project. The next day, <a href="https://irminsul.gg/">irminsul.gg</a> was up and running, along with its three subdomains for <a href="https://genshin.irminsul.gg/">Genshin</a>, <a href="https://hsr.irminsul.gg/">HSR</a>, and <a href="https://wuwa.irminsul.gg/">WuWa</a>.</Typography><br />
+            <Typography>Today marks the first official release of Irminsul.gg. I'm deciding to tag it as Version 0.5; its not 1.0 just yet because I still want to polish some stuff in the subsites first, and the previous four versions (0.1 - 0.4 if you will) represent the evolutions from the GIRS to Project Irminsul. It certainly has been a long journey, from a single little website in March 2021 to three comprehensive websites hosted on CloudFront in September 2024. I don't plan on stopping updating these sites anytime soon, and I have been thinking about making a fourth site for <a href="https://zenless.hoyoverse.com/en-us/main"><em>Zenless Zone Zero</em></a> in the future if time allows.</Typography><br />
+            <Typography>Truthfuly, I have no idea if anyone will actually use this site (other than myself and friends I show this to), but if it somehow gets popular and people need to contact me for feedback and/or to report issues, I can probably set something up for that. If you are reading this and made it to the end, thank you!</Typography><br />
+            <Typography>- BC (9/21/2024)</Typography>
+        </Box>
+    )
+
+}
+
+export default About
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 // Component imports
 import Nav from "./Nav"
+import About from "./About"
 import BottomNav from "./BottomNav"
 
 // MUI imports
@@ -14,157 +15,150 @@ function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <React.Fragment>
+            <Box
+                id="main"
+                sx={{
+                    pt: 15,
+                    height: "100vh",
+                    maxWidth: "100vw",
+                    backgroundImage: `linear-gradient(to bottom, ${theme.body.backgroundColor} 15%, rgba(73, 218, 243, 0.2) 50%, ${theme.body.backgroundColor} 100%), url(https://assets.irminsul.gg/main/images/Irminsul.png)`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "50% 50%"
+                }}
+            >
                 <Nav />
                 <Box
-                    id="main"
                     sx={{
-                        px: 5,
-                        pt: 15,
-                        minHeight: "100vh",
-                        backgroundImage: `linear-gradient(to bottom, ${theme.body.backgroundColor} 15%, rgba(73, 218, 243, 0.2) 50%, ${theme.body.backgroundColor} 100%), url(https://assets.irminsul.gg/main/images/Irminsul.png)`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "cover",
-                        backgroundPosition: "50% 50%"
+                        textAlign: "center",
+                        mb: "50px"
                     }}
                 >
-                    <Box
+                    <Typography
                         sx={{
-                            textAlign: "center",
-                            mb: "50px"
+                            color: `${theme.text.color}`,
+                            fontSize: "24pt",
+                            fontWeight: 400
                         }}
                     >
-                        <Typography
-                            sx={{
-                                color: `${theme.text.color}`,
-                                fontSize: "24pt",
-                                fontFamily: "Rowdies"
-                            }}
-                        >
-                            The database for gacha games.
-                        </Typography>
-                        <Typography
-                            sx={{
-                                color: `${theme.text.color}`,
-                                fontSize: "16pt",
-                                fontWeight: 300,
-                                fontFamily: "Rowdies"
-                            }}
-                        >
-                            Select a branch of Irminsul to view its database:
-                        </Typography>
-                    </Box>
-                    <Box
+                        The database for gacha games.
+                    </Typography>
+                    <Typography
                         sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignContent: "center",
+                            color: `${theme.text.color}`,
+                            fontSize: "16pt"
                         }}
                     >
-                        <Grid container spacing={5} sx={{ mb: "100px" }}>
-                            {
-                                websites.map((site, index) => (
-                                    <Grid
-                                        key={index}
-                                        size="auto"
-                                        sx={{
-                                            mx: "auto",
-                                            borderRadius: "15px",
-                                            backgroundColor: `${theme.card.backgroundColor}`,
-                                        }}
-                                    >
-                                        <ButtonBase disableRipple href={site.enabled ? site.href : ""}>
-                                            {/* I know you can do the below using the other MUI Card components (like CardMedia, CardContent), 
+                        Select a branch of Irminsul to view its database:
+                    </Typography>
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignContent: "center",
+                    }}
+                >
+                    <Grid container spacing={5} sx={{ mb: "300px", mx: "25px" }}>
+                        {
+                            websites.map((site, index) => (
+                                <Grid
+                                    key={index}
+                                    size="auto"
+                                    sx={{
+                                        mx: "auto",
+                                        borderRadius: "15px",
+                                        backgroundColor: `${theme.card.backgroundColor}`,
+                                    }}
+                                >
+                                    <ButtonBase disableRipple href={site.enabled ? site.href : ""}>
+                                        {/* I know you can do the below using the other MUI Card components (like CardMedia, CardContent), 
                                             but that resulted in some very tiny border clippings.
                                             You probably wouldn't notice if no one said anything,
                                             but it bothered me enough to spend an hour trying to fix it. */}
-                                            <Box
-                                                onMouseEnter={() => zoomOnHover("enter", site.tag, site.imageTransform)}
-                                                onMouseLeave={() => zoomOnHover("leave", site.tag, site.imageTransform)}
-                                                sx={
-                                                    site.enabled ?
-                                                        { opacity: 1, cursor: "pointer" }
-                                                        :
-                                                        { opacity: 0.5, cursor: "auto" }
-                                                }
+                                        <Box
+                                            onMouseEnter={() => zoomOnHover("enter", site.tag, site.imageTransform)}
+                                            onMouseLeave={() => zoomOnHover("leave", site.tag, site.imageTransform)}
+                                            sx={
+                                                site.enabled ?
+                                                    { opacity: 1, cursor: "pointer" }
+                                                    :
+                                                    { opacity: 0.5, cursor: "auto" }
+                                            }
+                                        >
+                                            <Card
+                                                sx={{
+                                                    width: "400px",
+                                                    height: "225px",
+                                                    borderRadius: "15px 15px 0px 0px",
+                                                    backgroundColor: `${theme.card.backgroundColor}`,
+                                                }}
                                             >
-                                                <Card
-                                                    sx={{
-                                                        width: "400px",
-                                                        height: "225px",
-                                                        borderRadius: "15px 15px 0px 0px",
-                                                        backgroundColor: `${theme.card.backgroundColor}`,
-                                                    }}
-                                                >
-                                                    <img
-                                                        id={`${site.tag.toLowerCase()}-image`}
-                                                        src={`https://assets.irminsul.gg/main/wallpapers/${site.tag}.png`}
-                                                        alt={`${site.tag}`}
-                                                        style={{
-                                                            position: "relative",
-                                                            zIndex: 0,
-                                                            width: "400px",
-                                                            height: "auto",
-                                                            aspectRatio: "16 / 9",
-                                                            transform: `scale(${site.imageTransform.scale}) translate(${site.imageTransform.translate[0]}px, ${site.imageTransform.translate[1]}px)`,
-                                                            transition: "transform .2s"
-                                                        }}
-                                                    />
-                                                </Card>
-                                                <Box
-                                                    sx={{
+                                                <img
+                                                    id={`${site.tag.toLowerCase()}-image`}
+                                                    src={`https://assets.irminsul.gg/main/wallpapers/${site.tag}.png`}
+                                                    alt={`${site.tag}`}
+                                                    style={{
                                                         position: "relative",
-                                                        zIndex: 1,
-                                                        display: "flex",
-                                                        justifyContent: "center",
-                                                        alignItems: "center",
-                                                        textAlign: "center",
-                                                        p: 2,
-                                                        mt: "-5px",
-                                                        height: "40px",
-                                                        borderTop: `5px solid ${theme.border.color}`,
-                                                        borderRadius: "0px 0px 15px 15px",
-                                                        backgroundColor: `${theme.appbar.backgroundColor}`,
-                                                        color: `${theme.text.color}`
+                                                        zIndex: 0,
+                                                        width: "400px",
+                                                        height: "auto",
+                                                        aspectRatio: "16 / 9",
+                                                        transform: `scale(${site.imageTransform.scale}) translate(${site.imageTransform.translate[0]}px, ${site.imageTransform.translate[1]}px)`,
+                                                        transition: "transform .2s"
                                                     }}
-                                                >
-                                                    <Box>
+                                                />
+                                            </Card>
+                                            <Box
+                                                sx={{
+                                                    position: "relative",
+                                                    zIndex: 1,
+                                                    display: "flex",
+                                                    justifyContent: "center",
+                                                    alignItems: "center",
+                                                    textAlign: "center",
+                                                    p: 2,
+                                                    mt: "-5px",
+                                                    height: "40px",
+                                                    borderTop: `5px solid ${theme.border.color}`,
+                                                    borderRadius: "0px 0px 15px 15px",
+                                                    backgroundColor: `${theme.appbar.backgroundColor}`,
+                                                    color: `${theme.text.color}`
+                                                }}
+                                            >
+                                                <Box>
+                                                    <Typography
+                                                        sx={{
+                                                            color: `${theme.text.color}`,
+                                                            fontSize: "15pt"
+                                                        }}
+                                                    >
+                                                        {site.title}
+                                                    </Typography>
+                                                    {
+                                                        !site.enabled &&
                                                         <Typography
                                                             sx={{
                                                                 color: `${theme.text.color}`,
-                                                                fontSize: "15pt",
-                                                                fontWeight: 300,
-                                                                fontFamily: "Rowdies"
+                                                                fontSize: "12pt"
                                                             }}
                                                         >
-                                                            {site.title}
+                                                            Coming soon!
                                                         </Typography>
-                                                        {
-                                                            !site.enabled &&
-                                                            <Typography
-                                                                sx={{
-                                                                    color: `${theme.text.color}`,
-                                                                    fontSize: "12pt",
-                                                                    fontWeight: 300,
-                                                                    fontFamily: "Rowdies"
-                                                                }}
-                                                            >
-                                                                Coming soon!
-                                                            </Typography>
-                                                        }
-                                                    </Box>
+                                                    }
                                                 </Box>
                                             </Box>
-                                        </ButtonBase>
-                                    </Grid>
-                                ))
-                            }
-                        </Grid>
-                    </Box>
+                                        </Box>
+                                    </ButtonBase>
+                                </Grid>
+                            ))
+                        }
+                    </Grid>
                 </Box>
+                <About />
                 <BottomNav />
-            </React.Fragment>
-        </ThemeProvider>
+            </Box>
+        </ThemeProvider >
     )
 
 }
