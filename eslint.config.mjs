221e03177866c3c6905ae73f9bdf6f251ed2e926
Brian <briantcheung@gmail.com>
2025-10-03 23:32:45 -0700

    Update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,20 +6,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+        ],
+        "rules": {
+            "no-restricted-imports": [
+                "error",
+                {
+                    "patterns": [{ "regex": "^@mui/[^/]+$" }]
+                }
+            ]
+        }
+    },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "assets.irminsul.gg",
+                port: "",
+                pathname: "/**",
+                search: "",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
