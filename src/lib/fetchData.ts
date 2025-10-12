691124e304e4ccef1461b62b43107560fb1831e0
Brian <briantcheung@gmail.com>
2025-10-12 02:10:40 -0700

    Update fetch

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -1,3 +1,21 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
 export type LoadingStatus = "idle" | "pending" | "success" | "error";
+
+export const urls: { [key: string]: string } = {
+    "genshin/characters": "https://api.irminsul.gg/genshin/characters.json",
+    "genshin/weapons": "https://api.irminsul.gg/genshin/weapons.json",
+    "genshin/artifacts": "https://api.irminsul.gg/genshin/artifacts.json",
+};
+
+export async function getDataSet<T>(url: string): Promise<T[]> {
+    const res = await fetch(urls[url]);
+    return res.json();
+}
+
+export async function getData<T>(
+    url: string,
+    params: (value: T) => unknown
+): Promise<T> {
+    const res = await fetch(urls[url]);
+    const data = await res.json();
+    return data.find(params);
+}
