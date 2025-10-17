975c9834d22678e91bd1bcc2897a2fb04d0902e5
Brian <briantcheung@gmail.com>
2025-10-17 13:57:05 -0700

    Consolidated tab components

---
+++ src/components/Tabs/TabList.tsx
@@ -0,0 +1,56 @@
+// MUI imports
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Tabs, { TabsProps } from "@mui/material/Tabs";
+
+interface CustomTabsProps extends TabsProps {
+    tabcolor?: string;
+}
+
+export default function TabList(props: CustomTabsProps) {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
+    const { children, value, onChange, sx, ...other } = props;
+
+    return (
+        <Tabs
+            value={value}
+            onChange={onChange}
+            variant="scrollable"
+            scrollButtons="auto"
+            allowScrollButtonsMobile={!matches}
+            sx={{
+                ...{
+                    height: "100%",
+                    "& .MuiTabs-indicator": {
+                        display: "flex",
+                        justifyContent: "center",
+                        backgroundColor: "transparent",
+                    },
+                    "& .MuiTabScrollButton-root": {
+                        color: theme.text.primary,
+                        backgroundColor: theme.background(2),
+                    },
+                    ".MuiTabs-scrollButtons.Mui-disabled": {
+                        opacity: 0.3,
+                    },
+                    "& .MuiTabs-indicatorSpan": {
+                        width: "100%",
+                        backgroundColor:
+                            props.tabcolor || theme.border.color.primary,
+                    },
+                },
+                ...sx,
+            }}
+            {...other}
+            slotProps={{
+                indicator: {
+                    children: <span className="MuiTabs-indicatorSpan" />,
+                },
+            }}
+        >
+            {children}
+        </Tabs>
+    );
+}
---
+++ src/components/Tabs/Tabs.ts
@@ -0,0 +1,9 @@
+import TabList from "./TabList";
+import List from "./TabList";
+import TabPanel from "./TabPanel";
+import Panel from "./TabPanel";
+import TabSelector from "./TabSelector";
+import Selector from "./TabSelector";
+
+export default { List, Panel, Selector };
+export { TabList, TabPanel, TabSelector };
