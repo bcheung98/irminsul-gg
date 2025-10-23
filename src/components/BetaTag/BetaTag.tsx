f2ade6909c5f3ee56cd5191a21f9fd49a37c6ac8
Brian <briantcheung@gmail.com>
2025-10-22 18:51:25 -0700

    Added beta tag

---
+++ src/components/BetaTag/BetaTag.tsx
@@ -0,0 +1,31 @@
+// Component imports
+import Text from "@/components/Text";
+
+// MUI imports
+import Card from "@mui/material/Card";
+
+// Helper imports
+import { useGameTag } from "@/app/context";
+import { isUreleasedContent } from "@/helpers/isUnreleasedContent";
+
+export default function BetaTag({ version }: { version: string }) {
+    const game = useGameTag();
+
+    const text =
+        game === "uma"
+            ? "This content is only available on the JP server!"
+            : "Viewing beta content, all content is subject to change!";
+
+    if (!isUreleasedContent(version, game)) {
+        return (
+            <Card
+                sx={(theme) => ({
+                    p: 2,
+                    backgroundColor: theme.palette.error.dark,
+                })}
+            >
+                <Text>{text}</Text>
+            </Card>
+        );
+    } else return null;
+}
