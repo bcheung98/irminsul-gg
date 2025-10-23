e011dfc0422bbee9d5cc75a2f406f8522335ad82
Brian <briantcheung@gmail.com>
2025-10-22 18:09:41 -0700

    Update types

--- src/helpers/isUnreleasedContent.ts
+++ src/helpers/isUnreleasedContent.ts
@@ -1,7 +1,7 @@
 import versions from "@/data/versions";
+import { Game } from "@/types";
 
-export function isUreleasedContent(version: string, game: string) {
-    const tag = game.toLocaleLowerCase();
-    if (tag === "uma") return version === "";
-    else return versions[tag].map((v) => v.version).includes(version);
+export function isUreleasedContent(version: string, game: Game) {
+    if (game === "uma") return version === "";
+    else return versions[game].map((v) => v.version).includes(version);
 }
