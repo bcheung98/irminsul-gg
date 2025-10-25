d987a2486c384a159dcb8d58622d15db537a03ae
Brian <briantcheung@gmail.com>
2025-10-24 20:16:22 -0700

    Added basic settings

--- src/components/Settings/SettingsList.tsx
+++ src/components/Settings/SettingsList.tsx
@@ -1,9 +1,47 @@
+"use client";
+
+import { BaseSyntheticEvent, useRef, useState } from "react";
+
+// Component imports
+import ToggleButtons from "@/components/ToggleButtons";
+
 // MUI imports
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 
+// Helper imports
+import { themeList } from "@/themes/theme";
+import { useSettingsStore } from "@/stores/useSettingsStore";
+
 export default function SettingsList() {
     const theme = useTheme();
 
-    return <Box></Box>;
+    const { theme: themeIndex, setTheme } = useSettingsStore();
+
+    const toggleButtonsParams = {
+        spacing: 0,
+        padding: "4px 8px",
+        highlightOnHover: false,
+    };
+
+    return (
+        <Box>
+            <ToggleButtons
+                buttons={themeButtons}
+                value={themeIndex}
+                exclusive
+                onChange={(_: BaseSyntheticEvent, newValue: number) => {
+                    if (newValue !== null) {
+                        setTheme(newValue);
+                    }
+                }}
+                {...toggleButtonsParams}
+            />
+        </Box>
+    );
 }
+
+const themeButtons = themeList.map((theme) => ({
+    value: theme.id,
+    label: theme.name,
+}));
