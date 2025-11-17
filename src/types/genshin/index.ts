e19747dfd43d65bc9bc5f3e422248b3b3759b8f6
Brian <briantcheung@gmail.com>
2025-11-17 11:28:57 -0800

    Update types

--- src/types/genshin/index.ts
+++ src/types/genshin/index.ts
@@ -5,9 +5,13 @@ import {
     nations,
     arkhe,
 } from "@/data/genshin/common";
+import { GenshinCharacter } from "./character";
+import { GenshinWeapon } from "./weapon";
 
 export type GenshinElement = (typeof elements)[number];
 export type GenshinWeaponType = (typeof weapons)[number];
 export type GenshinRarity = (typeof rarities)[number];
 export type GenshinNation = (typeof nations)[number];
 export type GenshinArkhe = (typeof arkhe)[number];
+
+export type { GenshinCharacter, GenshinWeapon };
