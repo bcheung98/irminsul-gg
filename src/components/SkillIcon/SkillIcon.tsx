544a40d45bacfb618cf316907573473f60f0ee7a
Brian <briantcheung@gmail.com>
2025-11-17 16:54:45 -0800

    Update params

--- src/components/SkillIcon/SkillIcon.tsx
+++ src/components/SkillIcon/SkillIcon.tsx
@@ -1,5 +1,5 @@
 // Component imports
-import Image from "../Image";
+import Image from "@/components/Image";
 
 // MUI imports
 import { useTheme } from "@mui/material/styles";
@@ -17,35 +17,42 @@ export default function SkillIcon({
     selected,
     size = 48,
     responsiveSize = 1 / 6,
+    padding = "4px",
+    backgroundColor,
     borderWidth = "2px",
+    borderRadius = "64px",
 }: {
     icon: string;
     attributes?: AttributeData;
     selected?: boolean;
     size?: number;
     responsiveSize?: number;
-    borderWidth?: string;
+    padding?: string | number;
+    backgroundColor?: string;
+    borderWidth?: string | number;
+    borderRadius?: string | number;
 }) {
     const theme = useTheme();
 
     const game = useGameTag();
 
-    const textColor = useTextColor(theme.text);
+    const color =
+        useTextColor(theme.text)(game, attributes?.element) ||
+        theme.border.color.primary;
 
     return (
         <Image
             src={icon}
             size={size}
             style={{
-                padding: "4px",
-                backgroundColor: theme.iconBackground.primary,
+                padding,
+                backgroundColor:
+                    backgroundColor || theme.iconBackground.primary,
                 borderWidth: selected ? "thick" : borderWidth,
                 borderStyle: selected ? "double" : "solid",
-                borderColor: textColor(game, attributes?.element),
-                borderRadius: "64px",
-                boxShadow: selected
-                    ? `0 0 12px 4px ${textColor(game, attributes?.element)}`
-                    : "none",
+                borderColor: color,
+                borderRadius,
+                boxShadow: selected ? `0 0 12px 4px ${color}` : "none",
                 transition: "box-shadow 250ms",
             }}
             responsive
