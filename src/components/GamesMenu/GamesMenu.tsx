18756ec21a942e0c7a47702e7ac896d3444b4623
Brian <briantcheung@gmail.com>
2025-11-20 22:49:11 -0800

    Adjust navbar items

--- src/components/GamesMenu/GamesMenu.tsx
+++ src/components/GamesMenu/GamesMenu.tsx
@@ -3,11 +3,14 @@ import { useState } from "react";
 // Component imports
 import GamesMenuList from "./GamesMenuList";
 import NavButton from "@/components/NavButton";
+import Text from "@/components/Text";
 
 // MUI imports
 import { useTheme } from "@mui/material/styles";
 import Menu from "@mui/material/Menu";
 import AppsIcon from "@mui/icons-material/Apps";
+import Button from "@mui/material/Button";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function GamesMenu() {
     const theme = useTheme();
@@ -23,10 +26,44 @@ export default function GamesMenu() {
 
     return (
         <>
+            <Button
+                onClick={handleMenuOpen}
+                variant="text"
+                endIcon={
+                    <KeyboardArrowDownIcon
+                        fontSize="small"
+                        sx={{
+                            transform: open
+                                ? "rotateZ(-180deg)"
+                                : "rotateZ(0deg)",
+                            transition: "transform 0.25s",
+                        }}
+                    />
+                }
+                sx={[
+                    {
+                        display: { xs: "none", md: "flex" },
+                        transition: "color 0.25s",
+                        "&:hover": {
+                            color: theme.text.selected,
+                            textShadow: ` ${theme.text.selected} 1px 1px 8px`,
+                        },
+                    },
+                    open && {
+                        color: theme.text.selected,
+                        textShadow: ` ${theme.text.selected} 1px 1px 8px`,
+                    },
+                ]}
+            >
+                <Text variant="body2" sx={{ color: "inherit" }}>
+                    Games
+                </Text>
+            </Button>
             <NavButton
                 onClick={handleMenuOpen}
                 title="Games"
                 sx={{
+                    display: { xs: "flex", md: "none" },
                     backgroundColor: open
                         ? theme.appbar.backgroundColor.selectedHover
                         : "transparent",
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,24 +7,31 @@ import SiteSearch from "@/components/SiteSearch";
 import Settings from "@/components/Settings";
 import FlexBox from "@/components/FlexBox";
 import NavButton from "@/components/NavButton";
+import NavLink from "@/components/NavLink";
+import Text from "@/components/Text";
 
 // MUI imports
-import { alpha } from "@mui/material/styles";
+import { alpha, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 // Helper imports
 import { useGameTag } from "@/context";
 
 export default function NavBar() {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up("md"));
+
     const game = useGameTag();
 
     return (
         <AppBar
             position="fixed"
-            sx={(theme) => ({
+            sx={{
                 containerType: "inline-size",
                 zIndex: theme.zIndex.drawer + 1,
                 backdropFilter: { sm: "none", lg: "blur(4px)" },
@@ -32,7 +39,7 @@ export default function NavBar() {
                     xs: theme.appbar.backgroundColor.main,
                     lg: alpha(theme.appbar.backgroundColor.main, 0.95),
                 },
-            })}
+            }}
         >
             <Toolbar
                 variant="dense"
@@ -61,7 +68,33 @@ export default function NavBar() {
                 >
                     <FlexBox spacing={1}>
                         <GamesMenu />
-                        <NavButton title="Calendar" href="/calendar">
+                        {matches && (
+                            <NavLink href="/calendar">
+                                <Button
+                                    variant="text"
+                                    sx={{
+                                        display: { xs: "none", md: "flex" },
+                                        transition: "color 0.25s",
+                                        "&:hover": {
+                                            color: theme.text.selected,
+                                            textShadow: ` ${theme.text.selected} 1px 1px 8px`,
+                                        },
+                                    }}
+                                >
+                                    <Text
+                                        variant="body2"
+                                        sx={{ color: "inherit" }}
+                                    >
+                                        Calendar
+                                    </Text>
+                                </Button>
+                            </NavLink>
+                        )}
+                        <NavButton
+                            title="Calendar"
+                            href="/calendar"
+                            sx={{ display: { xs: "flex", md: "none" } }}
+                        >
                             <CalendarMonthIcon />
                         </NavButton>
                     </FlexBox>
