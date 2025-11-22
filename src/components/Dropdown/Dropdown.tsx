a39bf7076879da637a74fd5704bf3ffa1d3f217b
Brian <briantcheung@gmail.com>
2025-11-22 12:45:17 -0800

    Update dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,84 +1,126 @@
-import { CSSProperties, useState } from "react";
+import { useState } from "react";
 
 // Component imports
-import Image from "../Image";
-import Text from "../Text";
+import FlexBox from "@/components/FlexBox";
+import TextLabel from "@/components/TextLabel";
 
 // MUI imports
-import { useTheme, ButtonBase, Collapse, Box } from "@mui/material";
-import { ExpandMore } from "@mui/icons-material";
-
-// Helper imports
-import { combineStyles } from "@/utils";
+import { useTheme, TypographyVariant } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ButtonBase from "@mui/material/ButtonBase";
+import { StackProps } from "@mui/material/Stack";
 
 interface DropdownProps {
     children?: React.ReactNode;
+    icon?: React.ReactNode;
     title?: string;
     titleColor?: string;
+    textVariant?: TypographyVariant;
     img?: string;
-    imgStyle?: CSSProperties;
+    imgStyle?: React.CSSProperties;
     iconColor?: string;
     contentPadding?: string | number;
     unmountOnExit?: boolean;
     defaultOpen?: boolean;
+    reverse?: boolean;
+    rotate?: number | [number, number] | false;
+    axis?: "X" | "Y" | "Z";
+    spacing?: string | number;
+    justifyContent?: StackProps["justifyContent"];
 }
 
 export default function Dropdown({
     children,
+    icon,
     title = "",
     titleColor,
+    textVariant = "subtitle1",
     img,
     imgStyle,
     iconColor,
     contentPadding = "4px 24px",
     unmountOnExit = false,
     defaultOpen = false,
+    reverse = false,
+    rotate,
+    axis = "Z",
+    spacing = 0.5,
+    justifyContent = "left",
 }: DropdownProps) {
     const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
     const [open, setOpen] = useState(defaultOpen);
     const toggleDropdownState = () => {
         setOpen(!open);
     };
 
+    let start: number;
+    let end: number;
+    if (rotate === false) {
+        (start = 0), (end = 0);
+    } else if (Array.isArray(rotate)) {
+        [start, end] = rotate;
+    } else {
+        (start = 0), (end = rotate ?? -90);
+    }
+
     return (
         <Box sx={{ mb: "8px" }}>
-            <ButtonBase
-                onClick={toggleDropdownState}
-                disableRipple
-                disableTouchRipple
+            <FlexBox
                 sx={{
                     maxWidth: "100%",
-                    pl: 0,
                     mb: "4px",
-                    justifyContent: "left",
+                    justifyContent: justifyContent,
+                    flexDirection: reverse ? "row-reverse" : "row",
                 }}
             >
-                <ExpandMore
+                <ButtonBase
+                    disableRipple
+                    onClick={toggleDropdownState}
                     sx={{
-                        mr: "4px",
+                        mr: !reverse && justifyContent === "left" ? spacing : 0,
                         color: iconColor || theme.border.color.primary,
-                        transform: open ? "rotateZ(0deg)" : "rotateZ(-90deg)",
+                        transform: open
+                            ? `rotate${axis}(${start}deg)`
+                            : `rotate${axis}(${end}deg)`,
                         transition: "transform 0.25s",
                     }}
-                />
-                {img && (
-                    <Image
-                        src={img}
-                        style={combineStyles(
-                            {
-                                width: "32px",
-                                height: "32px",
-                                marginRight: "8px",
-                            },
-                            imgStyle
-                        )}
+                >
+                    {icon || (
+                        <ExpandMoreIcon
+                            fontSize={matches ? "medium" : "small"}
+                        />
+                    )}
+                </ButtonBase>
+                <ButtonBase
+                    disableRipple
+                    onClick={toggleDropdownState}
+                    sx={{
+                        pl: !reverse && justifyContent === "left" ? 1 : 0,
+                        ml:
+                            !reverse && justifyContent === "left"
+                                ? -spacing
+                                : 0,
+                    }}
+                >
+                    <TextLabel
+                        icon={img}
+                        iconProps={{
+                            size: 32,
+                            styles: { ...imgStyle },
+                        }}
+                        title={title}
+                        titleProps={{
+                            color: titleColor || theme.text.primary,
+                            variant: textVariant,
+                        }}
                     />
-                )}
-                <Text sx={{ color: titleColor || theme.text.primary }} noWrap>
-                    {title}
-                </Text>
-            </ButtonBase>
+                </ButtonBase>
+            </FlexBox>
             <Collapse in={open} timeout="auto" unmountOnExit={unmountOnExit}>
                 <Box sx={{ p: { xs: "8px 0", md: contentPadding } }}>
                     {children}
