0eb73c4908b892bc11d76cb1e55f15fe587f971b
Brian <briantcheung@gmail.com>
2026-04-16 21:42:57 -0700

    Endfield - Version 1.2 Update

--- src/data/endfield/versions.ts
+++ src/data/endfield/versions.ts
@@ -1,6 +1,7 @@
 import { VersionInfo } from "@/types/version";
 
 export const endfieldVersions: VersionInfo[] = [
+    { version: "1.2", name: "At the Wake of Spring" },
     {
         version: "1.1",
         name: "Old Deep Water Dies, by Rising Tide It is Denied",
