e731831370b6d7f05636eb8d1c99aadc3e2dcb68
bcheung98 <briantcheung@gmail.com>
2026-05-30 20:47:40 -0700

    Update versions

--- src/data/nte/versions.ts
+++ src/data/nte/versions.ts
@@ -1,6 +1,6 @@
 import { VersionInfo } from "@/types/version";
 
 export const nteVersions: VersionInfo[] = [
-    { version: "1.1", name: "Dreamwalk Corridor" },
+    // { version: "1.1", name: "Dreamwalk Corridor" },
     { version: "1.0", name: "Neverness to Everness" },
 ];
