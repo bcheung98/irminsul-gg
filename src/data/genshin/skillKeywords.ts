6bc7d30e7123eddbb53bfc171f5355ef7d1ac9cd
bcheung98 <briantcheung@gmail.com>
2026-08-10 01:07:10 -0700

    Genshin - 7.0 Update

--- src/data/genshin/skillKeywords.ts
+++ src/data/genshin/skillKeywords.ts
@@ -13,4 +13,10 @@ export const skillKeywords: SkillKeyword[] = [
         description:
             'When Stellar-Conduct is triggered, the surrounding area will transform into a <span class="text-highlight">Polestar Field</span> for a short period of time, and when inside a Polestar Field, certain characters will enter the <span class="text-highlight">Radiance: Stellar-Conduct</span> state, which grants various skill buffs.',
     },
+    {
+        tag: "radiance stellar glimmer",
+        name: "Radiance: Stellar Glimmer",
+        description:
+            '<span class="text-highlight">Radiance: Stellar-Conduct</span> and <span class="text-highlight">Radiance: Stellar Swirl</span> are both considered <span class="text-highlight">Radiance: Stellar Glimmer</span> states.<br />Characters can only be affected by one Radiance state at any one time, and where more than one state can be triggered, <span class="text-highlight">Radiance: Stellar-Conduct</span> shall apply first.',
+    },
 ];
