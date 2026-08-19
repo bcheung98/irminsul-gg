069de21d8b6a24adde31382440f94a08a2a01a96
bcheung98 <briantcheung@gmail.com>
2026-08-18 18:18:11 -0700

    NTE - 1.3 Update

--- src/data/nte/versions.ts
+++ src/data/nte/versions.ts
@@ -1,6 +1,7 @@
 import { VersionInfo } from "@/types/version";
 
 export const nteVersions: VersionInfo[] = [
+    { version: "1.3", name: "Rising from the Moonlight Fog" },
     { version: "1.2", name: "999 Nights" },
     { version: "1.1", name: "Dreamwalk Corridor" },
     { version: "1.0", name: "Neverness to Everness" },
