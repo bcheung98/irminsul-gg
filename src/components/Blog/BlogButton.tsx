54dab26e551b3ba17f2e95e3fb586b08d4bc4063
bcheung98 <briantcheung@gmail.com>
2026-08-30 15:34:59 -0700

    Refactor responsive rendering

--- src/components/Blog/BlogButton.tsx
+++ src/components/Blog/BlogButton.tsx
@@ -7,55 +7,54 @@ import Text from "@/components/Text";
 
 // MUI imports
 import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import Button from "@mui/material/Button";
 import ArticleIcon from "@mui/icons-material/Article";
 
 export default function BlogButton() {
     const theme = useTheme();
-    const matches = useMediaQuery(theme.breakpoints.up("md"));
 
     const href = "/blog";
     const pathname = usePathname();
     const linkActive = pathname.includes("blog");
 
-    return matches ? (
-        <NavLink href={href}>
-            <Button
-                variant="text"
-                sx={{
-                    display: { xs: "none", md: "flex" },
-                    transition: "color 0.25s",
-                    color: linkActive
-                        ? theme.text.selected
-                        : theme.text.primary,
-                    textShadow: linkActive
-                        ? `${theme.text.selected} 1px 1px 8px`
-                        : "none",
-                    "&:hover": {
-                        color: theme.text.selected,
-                        textShadow: `${theme.text.selected} 1px 1px 8px`,
-                    },
-                }}
-            >
-                <Text
-                    variant="body2"
-                    weight="highlight"
+    return (
+        <>
+            <NavLink href={href}>
+                <Button
+                    variant="text"
                     sx={{
-                        color: "inherit",
+                        display: { xs: "none", md: "flex" },
+                        transition: "color 0.25s",
+                        color: linkActive
+                            ? theme.text.selected
+                            : theme.text.primary,
+                        textShadow: linkActive
+                            ? `${theme.text.selected} 1px 1px 8px`
+                            : "none",
+                        "&:hover": {
+                            color: theme.text.selected,
+                            textShadow: `${theme.text.selected} 1px 1px 8px`,
+                        },
                     }}
                 >
-                    Blog
-                </Text>
-            </Button>
-        </NavLink>
-    ) : (
-        <NavButton
-            title="Blog"
-            href={href}
-            sx={{ display: { xs: "flex", md: "none" } }}
-        >
-            <ArticleIcon />
-        </NavButton>
+                    <Text
+                        variant="body2"
+                        weight="highlight"
+                        sx={{
+                            color: "inherit",
+                        }}
+                    >
+                        Blog
+                    </Text>
+                </Button>
+            </NavLink>
+            <NavButton
+                title="Blog"
+                href={href}
+                sx={{ display: { xs: "flex", md: "none" } }}
+            >
+                <ArticleIcon />
+            </NavButton>
+        </>
     );
 }
--- src/components/KofiButton/KofiButton.tsx
+++ src/components/KofiButton/KofiButton.tsx
@@ -2,17 +2,10 @@
 import TextLabel from "@/components/TextLabel";
 
 // MUI imports
-import useMediaQuery from "@mui/material/useMediaQuery";
 import ButtonBase from "@mui/material/ButtonBase";
 import Avatar from "@mui/material/Avatar";
 
 export default function KofiButton() {
-    const matches_up_lg = useMediaQuery((theme) => theme.breakpoints.up("lg"));
-    const matches_dn_md = useMediaQuery((theme) =>
-        theme.breakpoints.down("md"),
-    );
-    const matches = matches_up_lg || matches_dn_md;
-
     return (
         <ButtonBase
             href="https://ko-fi.com/bcheung"
@@ -37,8 +30,17 @@ export default function KofiButton() {
                         sx={{ width: "auto", height: "20px" }}
                     />
                 }
-                title={matches && "Ko-Fi"}
-                titleProps={{ variant: "subtitle2" }}
+                title="Ko-Fi"
+                titleProps={{
+                    variant: "subtitle2",
+                    sx: {
+                        display: {
+                            xs: "inline",
+                            md: "none",
+                            lg: "inline",
+                        },
+                    },
+                }}
             />
         </ButtonBase>
     );
