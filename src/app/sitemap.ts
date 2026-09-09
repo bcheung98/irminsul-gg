5a8dc561c18a4e1a6f7690313e696d47d463d13d
bcheung98 <briantcheung@gmail.com>
2026-09-08 22:11:23 -0700

    Add revalidation for sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,8 @@ import { getSitemapPaths } from "@/utils/sitemap";
 
 const BASE_URL = "https://irminsul.gg";
 
+export const revalidate = 3600;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const paths = await getSitemapPaths();
     return paths.map((path) => ({
